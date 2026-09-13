2fe9a7eae5699c2ab61b5ebcd5fae97988eac2e3
THIRU24x <thiru10242006@gmail.com>
2026-09-14 00:18:17 +0530

    Add Counter App

---
+++ script.js
@@ -0,0 +1,17 @@
+
+ var  h1=document.getElementById("count")
+ let score=0
+ function i(){
+  score=score+1
+  h1.textContent=score
+
+ }
+ function d(){
+  score=score-1
+  h1.textContent=score
+
+ }
+ function r(){
+  score=0
+  h1.textContent=score
+ }
